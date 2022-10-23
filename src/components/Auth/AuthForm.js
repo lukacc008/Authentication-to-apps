1ad9e07a8869750831d3e1aaddf2637d7f917082
unknown <lukacc008@gmail.com>
2022-10-23 15:30:35 +0200

    Showing feedback to the user!

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -6,6 +6,7 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false)
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -19,8 +20,9 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
   
-  if (isLogin) {
+  setIsLoading(true);
 
+  if (isLogin) {
   } else {
     fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB8Adpfdpzwd6yaV8wWFQdxPYOI4telEv4',
       {
@@ -35,11 +37,17 @@ const AuthForm = () => {
         }
       }
     ).then(res => {
+      setIsLoading(false)
       if (res.ok) {
 
       } else {
-        res.json().then(data => {
-          console.log(data);
+        return res.json().then(data => {
+
+          let errorMessage = 'Authentication failed!';
+         // if (data && data.error && data.error.message) {
+         //     errorMessage = data.error.message;
+         // }
+          alert(errorMessage);
         });
       }
     });    
@@ -60,7 +68,8 @@ const AuthForm = () => {
           <input type='password' id='password' required  ref={passwordInputRef}/>
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request..</p>}
           <button
             type='button'
             className={classes.toggle}
