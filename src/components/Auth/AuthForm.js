32909bb23cf28be53f89daea010badb3cfd39813
unknown <lukacc008@gmail.com>
2022-10-23 14:35:07 +0200

    sending a POST request with fetch API and creating a new account on demo app with Firebase (Firebase docs and special key from project settings -> web API key.)

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -13,15 +13,44 @@ const AuthForm = () => {
  
   const submitHandler = (event) => {
     event.preventDefault();
-  };
 
-  const enteredEmail = emailInputRef.current.value;
-  const enteredPassword = passwordInputRef.current.value;
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+  
+  if (isLogin) {
+
+  } else {
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB8Adpfdpzwd6yaV8wWFQdxPYOI4telEv4',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(res => {
+      if (res.ok) {
+
+      } else {
+        res.json().then(data => {
+          console.log(data);
+        });
+      }
+    });    
+  }
+};
+
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
           <input type='email' id='email' required  ref={emailInputRef}/>
